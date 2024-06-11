8abebaa4e047a8c5b6eef54edc8ba076ca52998f
sraghavan-drio <sraghavan@drio.ai>
2024-06-10 19:36:24 -0700

    Caller can initialize channel on websocket handler init to receive client requests

--- server/server.go
+++ server/server.go
@@ -93,7 +93,8 @@ func (s *Server) Serve(l net.Listener) error {
 }
 
 // Serve WebSocket connections. Will receive a net.Conn object.
-func (s *Server) ServeWebSocketConnection(rw net.Conn) error {
+// Caller can pass a channel through which it will receive requests sent by the client.
+func (s *Server) ServeWebSocketConnection(rw net.Conn, ch chan client.Request) error {
 	if s.Log == nil {
 		s.Log = log.StdLogger{}
 	}
